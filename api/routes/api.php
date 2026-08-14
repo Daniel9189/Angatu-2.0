1b59784d612d4ff1826e3ca7e99a82bff024c58d
Daniel <dbrito4321@gmail.com>
2026-08-14 15:17:40 -0300

    feat: implement system authentication

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
@@ -12,5 +13,14 @@ Route::get('/user', function (Request $request) {
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{slug}', [ProductController::class, 'show'])->name('products.show');
 
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
+
+});
